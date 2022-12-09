af690e7c023cfa917f49eec09ec0a108dbaa2660
egorcouldlie <couldliegor@gmail.com>
2022-12-09 18:43:37 +0300

    homeWork6 task 1

--- src/HomeWork/Main.java
+++ src/HomeWork/Main.java
@@ -5,6 +5,17 @@ public class Main {
     task1();
     }
     public static void task1(){
-        
+        //task 1
+        int salaryPersonHasPerMonth = 15000;
+        int sum = 0;
+        int months = 1;
+        int percents;
+        do{
+            percents = sum / 100;
+            sum = sum + salaryPersonHasPerMonth + percents;
+            months++;
+        }
+        while (sum < 2477198);
+        System.out.println("Month " + months + " ,sum is " + sum);
     }
 }
