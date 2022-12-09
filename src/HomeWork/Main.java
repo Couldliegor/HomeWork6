136f5a844c4426031930b71a14031755e8b81b5f
egorcouldlie <couldliegor@gmail.com>
2022-12-09 18:50:21 +0300

    homeWork6 task 2

--- src/HomeWork/Main.java
+++ src/HomeWork/Main.java
@@ -3,6 +3,7 @@ package HomeWork;
 public class Main {
     public static void main(String[] args) {
     task1();
+    task2();
     }
     public static void task1(){
         //task 1
@@ -17,5 +18,19 @@ public class Main {
         }
         while (sum < 2477198);
         System.out.println("Month " + months + " ,sum is " + sum);
+    }
+    public static void task2(){
+        //task 2
+        int max = 10;
+        int min = 1;
+        while(min <= 10){
+            System.out.print(min + " ");
+            min++;
+        }
+        System.out.println();
+        for(;max >= 1;){
+            System.out.print(max + " ");
+            max--;
+        }
     }
 }
